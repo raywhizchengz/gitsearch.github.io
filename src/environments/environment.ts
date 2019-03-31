286e6ab5d82daadb9b34ec3a6520e1ad15b62839
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 21:45:52 +0300

    Revert environment production to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: "https://api.github.com/users/",
   access_token: "590681e7e1f38e16bda1807521c71620f3e8e55f"
 };
