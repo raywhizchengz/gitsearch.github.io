b680bf16ac141f86cc3fb343e837567d433f3fd7
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 20:20:08 +0300

    Add date count pipe code to calculate day difference

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,19 +8,19 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): number {
     let today:any = new Date();
 
-    // var difference =Math.abs(today-value)
-    // const secondsPerDay= 86400;
+    var difference =Math.abs(today-value)
+    const secondsPerDay= 86400;
 
-    // var dateDifferenceSeconds=difference*0.001;
+    var dateDifferenceSeconds=difference*0.001;
 
-    // var dateCount = Math.round(dateDifferenceSeconds/secondsPerDay);
+    var dateCount = Math.round(dateDifferenceSeconds/secondsPerDay);
 
 
-    // if (dateCount >= 1){
-    //     return dateCount - 1;
-    // }else{
-    //     return 0;
-    // }
+    if (dateCount >= 1){
+        return dateCount - 1;
+    }else{
+        return 0;
+    }
   }
 
 }
