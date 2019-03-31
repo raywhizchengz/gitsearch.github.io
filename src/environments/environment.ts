d256cb56fbd6a5404b076294e5fa1afd8fb0af9d
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 21:38:37 +0300

    set environment production to true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: "https://api.github.com/users/",
   access_token: "590681e7e1f38e16bda1807521c71620f3e8e55f"
 };
