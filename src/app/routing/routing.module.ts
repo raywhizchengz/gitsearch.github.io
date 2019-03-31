52ef1eaaca81a2bf9ea1925a208218fc610d64dd
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 20:25:17 +0300

    Define all required routes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -5,10 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { DisplayComponent } from '../display/display.component'
 import { RepositoryComponent } from '../repository/repository.component'
 
-// const routes: Routes = [
-//   {path:"display",component:DisplayComponent},
-//   {path:"repo",component:RepositoryComponent}
-// ]
+const routes: Routes = [
+  {path:"display",component:DisplayComponent},
+  {path:"repo",component:RepositoryComponent}
+]
 
 @NgModule({
   declarations: [],
