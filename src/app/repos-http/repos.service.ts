bd11a099919ca67a242cdd721c66c78853478b0a
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 20:35:32 +0300

    create service to consume Api that fetches repositories

--- src/app/repos-http/repos.service.ts
+++ src/app/repos-http/repos.service.ts
@@ -16,45 +16,45 @@ export class ReposService {
     this.repo = new Repository("","","","","");
   }
   
-    // repoRequest(){
+    repoRequest(){
 
-    //   interface ApiResponse{
-    //     html_url: any;
-    //     name: string;
-    //     description: any;
-    //     forks: number;
-    //     license: any;
-    //   }
-    //   let promise = new Promise((resolve,reject)=>{
-    //     this.http.get<ApiResponse>(environment.apiUrl + this.userName + this.repoName + "?access_tpken=" + environment.access_token).toPromise().then(response=>{
+      interface ApiResponse{
+        html_url: any;
+        name: string;
+        description: any;
+        forks: number;
+        license: any;
+      }
+      let promise = new Promise((resolve,reject)=>{
+        this.http.get<ApiResponse>(environment.apiUrl + this.userName + this.repoName + "?access_tpken=" + environment.access_token).toPromise().then(response=>{
 
-    //       this.repo.link = response.html_url
-    //       this.repo.name = response.name
-    //       this.repo.description
-    //        = response.description
-    //       this.repo.forks = response.forks
-    //       this.repo.license = response.license
+          this.repo.link = response.html_url
+          this.repo.name = response.name
+          this.repo.description
+           = response.description
+          this.repo.forks = response.forks
+          this.repo.license = response.license
           
-    //     resolve();
+        resolve();
 
-    //   },
-    //   error=>{
+      },
+      error=>{
 
-    //     this.repo.link = ""
-    //     this.repo.name = ""
-    //     this.repo.description
-    //      = ""
-    //     this.repo.forks = ""
-    //     this.repo.license = ""
+        this.repo.link = ""
+        this.repo.name = ""
+        this.repo.description
+         = ""
+        this.repo.forks = ""
+        this.repo.license = ""
         
-    //     reject(error);
+        reject(error);
 
 
-    //     })
-    //   })
+        })
+      })
 
-    //   return promise
+      return promise
 
-    // }
+    }
 
 }
