9188089fe1b3d9d42fd1cd40f24dffbdfe370056
fuaad001 <husseinfuaad@hotmail.com>
2019-03-31 20:18:04 +0300

    Add gitHub ApiURL and Access key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: "https://api.github.com/users/",
-  // access_token: "590681e7e1f38e16bda1807521c71620f3e8e55f"
+  apiUrl: "https://api.github.com/users/",
+  access_token: "590681e7e1f38e16bda1807521c71620f3e8e55f"
 };
 
 /*
